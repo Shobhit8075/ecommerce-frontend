7519e52aec87faee0eb13ffbe10035164ebf0036
Shobhit <144564108+Shobhit8075@users.noreply.github.com>
2025-12-26 11:48:58 +0530

    Add hero section and responsive header

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,9 @@
 console.log("E-Commerce Website Loaded");
+// Select hamburger and nav menu
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+// Toggle menu on click
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
